201c221171fd0b817de83eb7d07d43315143012a
Maximillllian <37188700+Maximillllian@users.noreply.github.com>
2020-10-24 17:24:22 +0500

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,3 +1,5 @@
-def test_store_button():
-  button = browser.find_elements_by_css_selector('button[type='submit'].btn-add-to-basket')
+def test_store_button(browser):
+  link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+  browser.get(link)
+  button = browser.find_elements_by_css_selector('button[type="submit"].btn-add-to-basket')
   assert button
